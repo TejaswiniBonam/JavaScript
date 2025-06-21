dc4a9aabe2e1e806435d6e92d4616be353989595
TejaswiniBonam <130749202+TejaswiniBonam@users.noreply.github.com>
2025-06-21 15:20:43 +0530

    update
    Co-Authored-By: Hari Charan <177325893+haricharanbonam@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
-document.write();
-const status = document.querySelector('.status');
+function f1(callback){
+    setTimeout(()=>{
+        console.log("TASK 1");
+        callback();
+    }, 3000);
+    
+}
+
+function f2(callback){
+    setTimeout(()=>{
+        console.log("TASK 2");
+        callback();
+    }, 3000);
+   
+}
+
+function f3(callback){
+    setTimeout(()=>{
+        console.log("TASK 3");
+        callback();
+    }, 3000);
+
+}
+
+function f4(callback){
+    setTimeout(()=>{
+        console.log("TASK 4");
+        callback();
+    }, 3000);
+
+}
+
+
+/*f1(
+    f2(
+        f3(
+            f4(
+                ()=>{
+                console.log("ALL");
+                }
+            )))); */
+
+f1(()=>{
+    f2(()=>{
+        f3(()=>{
+            f4(()=>{
+                console.log("ALL");
+            });
+        });
+    });
+});
