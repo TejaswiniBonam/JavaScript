79527a411cd8c670c060cb4063140a8e0c84fdc4
TejaswiniBonam <130749202+TejaswiniBonam@users.noreply.github.com>
2025-06-26 17:48:51 +0530

    update

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
-function f1(callback){
-    setTimeout(()=>{
-        console.log("TASK 1");
-        callback();
-    }, 3000);
+// function f1(callback){
+//     setTimeout(()=>{
+//         console.log("TASK 1");
+//         callback();
+//     }, 3000);
     
-}
+// }
 
-function f2(callback){
-    setTimeout(()=>{
-        console.log("TASK 2");
-        callback();
-    }, 3000);
+// function f2(callback){
+//     setTimeout(()=>{
+//         console.log("TASK 2");
+//         callback();
+//     }, 3000);
    
-}
+// }
 
-function f3(callback){
-    setTimeout(()=>{
-        console.log("TASK 3");
-        callback();
-    }, 3000);
+// function f3(callback){
+//     setTimeout(()=>{
+//         console.log("TASK 3");
+//         callback();
+//     }, 3000);
 
-}
+// }
 
-function f4(callback){
-    setTimeout(()=>{
-        console.log("TASK 4");
-        callback();
-    }, 3000);
+// function f4(callback){
+//     setTimeout(()=>{
+//         console.log("TASK 4");
+//         callback();
+//     }, 3000);
 
-}
+// }
 
 
-/*f1(
-    f2(
-        f3(
-            f4(
-                ()=>{
-                console.log("ALL");
-                }
-            )))); */
+// /*f1(
+//     f2(
+//         f3(
+//             f4(
+//                 ()=>{
+//                 console.log("ALL");
+//                 }
+//             )))); */
 
-f1(()=>{
-    f2(()=>{
-        f3(()=>{
-            f4(()=>{
-                console.log("ALL");
-            });
-        });
-    });
-});
+// f1(()=>{
+//     f2(()=>{
+//         f3(()=>{
+//             f4(()=>{
+//                 console.log("ALL");
+//             });
+//         });
+//     });
+// });
+// console.log("HIO");
+// let x;
+// //console.log(" x value : ", x);
+// //x = ;
+// //let x = 1;
+// console.log(" x value : ", x);
+// // TO know types of values in js 
+// // typeof
+// console.log(typeof x);
+
+// THREE WAYS to create a strning js
+let s1 = "abc";
+let s2 = 'def';
+let s3 = `ghi`; //backticks
+console.log(typeof s1, '\n', s2, '\n', s3);
+
+
+
+
+
+
